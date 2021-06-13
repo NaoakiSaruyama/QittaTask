f427a1e4e5521cbd64cff503f953d900c9c2474a
NaoakiSaruyama <saruyanamaoaki3412@gmail.com>
2021-06-13 10:33:43 +0900

    first-message

--- cms/urls.py
+++ cms/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.urls import path
 from cms import views
 
-app_name='cms'
+app_name = 'cms'
 urlpatterns=[
   path('book/',views.book_list,name='book_list'),
   path('book/add/',views.book_edit,name='book_add'),
